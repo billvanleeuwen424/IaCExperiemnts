35e470dbb535f3dda29fbe848aee930a7478b359
Bill Van Leeuwen <billvanleeuwen424@gmail.com>
2022-07-30 17:33:39 -0400

    pretty sweet terraform file

---
+++ terraform-files/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  shared_config_files = ["awscredentials"]
+  region              = "us-east-1"
+}
+
+
+resource "aws_instance" "app_server" {
+  ami             = "ami-06640050dc3f556bb"
+  instance_type   = "t2.large"
+  security_groups = ["MyGroup"]
+  tags = {
+    Name = "Gitlab Server"
+  }
+
+  root_block_device {
+    volume_size = 20
+  }
+
+  key_name = "AWS Key"
+}
